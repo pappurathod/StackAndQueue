b241e6100cd733b4eb50d52448229b431ced6b34
pappurathod <prathod4545@gmail.com>
2022-03-27 12:00:24 +0530

    Create Stack

---
+++ StackQueue/StackQueue/LinkedList.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackQueue
+{
+    internal class LinkedList
+    {
+        public Node Head;
+        public Node Tail;
+
+
+        public LinkedList()
+        {
+            Head = null;
+            Tail = null;
+        }
+        public void AddNode(Node node)
+        {
+            if (Head == null && Tail == null)
+            {
+                Head = node;
+                Tail = node;
+            }
+            else
+            {
+                node.next = Head;
+                Head = node;
+            }
+        }
+        public void Display()
+        {
+            Node temp = Head;
+            if (temp == null)
+            {
+                Console.WriteLine("empty");
+            }
+            else
+            {
+                Console.WriteLine("Stack as follows");
+
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data);
+                if (temp.next != null)
+                {
+                    Console.WriteLine("");
+                }
+                temp = temp.next;
+            }
+        }
+    }
+}
---
+++ StackQueue/StackQueue/Stack.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackQueue
+{
+    class Stack
+    {
+        LinkedList linkedList;
+        public Stack()
+        {
+            linkedList = new LinkedList();
+        }
+        public void Push(Node node)
+        {
+            linkedList.AddNode(node);
+        }
+        public void DisplayStack()
+        {
+            //Console.WriteLine("Stack");
+            linkedList.Display();
+        }
+        public void Top()
+        {
+            var val = linkedList.Head;
+            Console.WriteLine(val.data);
+        }
+    }
+}
